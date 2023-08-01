e623b3a18885f175e39be7fdde29e279d4c408ca
Shakil Chowdhury <shakil2chowdhury@gmail.com>
2023-08-01 23:50:46 +0600

    stripe updated

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: Request){
             }
         })
     }
-    if (event.type === "invoice.payment.succeeded") {
+    if (event.type === "invoice.payment_succeeded") {
         const subscription = await stripe.subscriptions.retrieve(
             session.subscription as string,
         )
